56e1c88c1d0196b62ceec80be026462f061653e4
DarwinCasasola14 <you@example.com>
2025-04-08 19:08:16 -0700

    JSF.Lab2

---
+++ Module3/JSFundamentals/Exercise2.js
@@ -0,0 +1,19 @@
+let three = "3";
+let four = "4";
+let thirty = "30";
+
+//what is the value of the following expression?
+let addition = Number(three) + Number(four);
+let multiplication = three * four;
+let division = three / four;
+let subtraction = three - four;
+let lessThan1 = three < four;
+let lessThan2 = Number(thirty) < Number(four);
+
+//Log the results
+console.log(addition);
+console.log(multiplication);
+console.log(division);
+console.log(subtraction);
+console.log(lessThan1);
+console.log(lessThan2);
