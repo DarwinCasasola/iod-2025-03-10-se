ab07e72c4b72c3bf8226050925459e3bd0594522
DarwinCasasola14 <you@example.com>
2025-07-19 11:18:22 -0700

    server udpate

--- Module9/mysqldb-app/server.js
+++ Module9/mysqldb-app/server.js
@@ -1,3 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// Import routes
 const postRoutes = require("./routes/postRoutes");
 
+// Middleware
+app.use(express.json());
+
+// Use routes
 app.use("/api/posts", postRoutes);
+
+// Server
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
